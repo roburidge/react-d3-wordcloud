2c2d5c367afaac7a2e8d28b54ee1876cfc887d3e
Rob Uridge <ruridge@gmail.com>
2017-04-09 16:19:07 +0100

    document utils

--- src/lib/utilities.js
+++ src/lib/utilities.js
@@ -1,5 +1,12 @@
 import * as d3 from 'd3'
 
+/**
+ * Returns the font size for the input volume
+ * @param {number} volume - volume we want to find fornt size for
+ * @param {array} volumeRange - range to map font sizes against
+ * @param {array} fontSizes - potential font sizes
+ * @returns {number} font size after mapping range brackes to sizes
+ */
 export function getFontSize (volume, volumeRange, fontSizes) {
     const quantizeSizes = d3.scaleQuantize()
         .domain(volumeRange)
@@ -8,6 +15,12 @@ export function getFontSize (volume, volumeRange, fontSizes) {
     return quantizeSizes(volume)
 }
 
+/**
+ * Returns red if score is less than 40, green if greater than 60
+ * otherwise returns gray
+ * @param {number} score
+ * @returns {string} color
+ */
 export function getFontColor (score) {
     switch (true) {
         case score < 40:
@@ -19,6 +32,12 @@ export function getFontColor (score) {
     }
 }
 
+/**
+ * returns function for setState
+ * @param {array} topics
+ * @param {array} fontSizes
+ * @returns {function}
+ */
 export function updateCloudData (topics, fontSizes) {
     return (prevState, props) => {
         const volumeRange = d3.extent(topics, (d) => d.volume)
