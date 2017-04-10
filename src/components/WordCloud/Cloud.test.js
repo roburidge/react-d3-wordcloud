add3e35e10cedc0b21b06cf776c4bc77d230786f
Rob Uridge <ruridge@gmail.com>
2017-04-10 11:57:56 +0100

    cloud and word tests

--- src/components/WordCloud/Cloud.test.js
+++ src/components/WordCloud/Cloud.test.js
@@ -3,21 +3,29 @@ import ReactDOM from 'react-dom'
 import { shallow } from 'enzyme'
 import {Cloud, Word} from './'
 
+const wrapper = shallow(
+    <Cloud width={500} height={500}><text /></Cloud>
+)
+
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(
       <Cloud
         width={500}
-        height={500}><div/></Cloud>
+        height={500}><text/></Cloud>
     , div)
 })
 
-it('renders text node', () => {
-    const wrapper = shallow(
-        <Cloud
-        width={500}
-        height={500}><text>hi</text></Cloud>
-    )
-    const text = <text>hi</text>
-    expect(wrapper.contains(text)).toEqual(true)
+it('to render child', () => {
+    expect(wrapper.contains(<text />)).toBe(true)
+})
+
+it('to have width and height props', () => {
+    expect(wrapper.prop('width')).toBe(500)
+    expect(wrapper.prop('height')).toBe(500)
+})
+
+it('to have group transform prop equal to 50% width, height', () => {
+    expect(wrapper.find('g').prop('transform')).toBe('translate(250, 250)')
+    
 })
--- src/components/WordCloud/Word.test.js
+++ src/components/WordCloud/Word.test.js
@@ -14,13 +14,27 @@ it('renders without crashing', () => {
     , div)
 })
 
-it('renders text node', () => {
-  const wrapper = shallow(
+const word = 'Topic'
+const wrapper = shallow(
       <Word
         x={10} y={10}
         fontSize={20}
         fontFamily="Arial"
-        color="rebeccapurple">Topic</Word>)
-  const word = 'Topic'
+        color="rebeccapurple">{word}</Word>)
+
+it('renders text node', () => {
   expect(wrapper.contains(word)).toEqual(true)
+})
+
+it('to have expected props', () => {
+    expect(wrapper.prop('className')).toBe('c-text')
+    expect(wrapper.prop('style')).toHaveProperty('fontSize', 20)
+    expect(wrapper.prop('style')).toHaveProperty('fontFamily', 'Arial')
+    expect(wrapper.prop('style')).toHaveProperty('fill', 'rebeccapurple')
+    expect(wrapper.prop('transform')).toBe('translate(10, 10) rotate(0)')
+    expect(wrapper.prop('textAnchor')).toBe('middle')
+})
+
+it('to be a text element', () => {
+    expect(wrapper.matchesElement(<text>{word}</text>)).toEqual(true)
 })
