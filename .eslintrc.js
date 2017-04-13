f4ab69fe3fe15cfb542cfa7df5c313cfdbe91ff1
Rob Uridge <ruridge@gmail.com>
2017-04-13 01:31:14 +0100

    eslint tidy up

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,29 @@
-{
+module.exports = {
+  // Prevent parent eslintrc from interfering
+  "root": true,
   "extends": [
     "eslint:recommended",
     "plugin:react/recommended",
     "prettier",
     "prettier/react"
   ],
-  "plugins": [
-    "react",
-    "prettier"
-  ],
+  "env": {
+    "es6": true,
+    "jest": true
+  },
   "parser": "babel-eslint",
   "parserOptions": {
-    "ecmaVersion": 2016,
+    "ecmaVersion": 2017,
     "sourceType": "module",
     "ecmaFeatures": {
       "jsx": true
     }
   },
-  "env": {
-    "es6": true,
-    "node": true
-  },
+  "plugins": [
+    "react",
+    "prettier"
+    // TODO: consider adding jsx-a11y and import
+  ],
   "rules": {
     "prettier/prettier": "error"
   }
